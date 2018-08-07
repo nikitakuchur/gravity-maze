66ba648f9b5e9428c58e1893cc55ba60e296b589
Nikita Kuchur <nikitakuchur@gmail.com>
2018-08-07 17:33:48 +0300

    Initial commit

---
+++ core/src/com/android/game/PuzzleGame.java
@@ -0,0 +1,11 @@
+package com.android.game;
+
+import com.badlogic.gdx.Game;
+
+public class PuzzleGame extends Game {
+	
+	@Override
+	public void create () {
+	    this.setScreen(new GameScreen());
+	}
+}
---
+++ desktop/src/com/android/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.android.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.android.game.PuzzleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 800;
+		config.width = 450;
+		new LwjglApplication(new PuzzleGame(), config);
+	}
+}
