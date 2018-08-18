fcc32534b5c72e72c90057b7d07ae57a4824d10d
Nikita Kuchur <nikitakuchur@gmail.com>
2018-08-18 14:16:15 +0300

    Added Level class

---
+++ core/src/com/android/game/model/Level.java
@@ -0,0 +1,27 @@
+package com.android.game.model;
+
+public class Level {
+    private int[][] map =   {{0, 1, 1, 1, 1, 1, 0, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 1},
+                            {0, 0, 0, 0, 0, 0, 0, 1},
+                            {1, 0, 0, 1, 1, 0, 1, 1},
+                            {1, 0, 0, 1, 1, 0, 1, 1},
+                            {1, 0, 0, 0, 0, 0, 0, 0},
+                            {1, 0, 0, 0, 0, 0, 0, 1},
+                            {1, 1, 0, 0, 0, 0, 1, 1}};
+
+    public Level() {
+    }
+
+    public int getWidth() {
+        return map[0].length;
+    }
+
+    public int getHeight() {
+        return map.length;
+    }
+
+    public int getCellId(int x, int y) {
+        return map[map.length - 1 - x][y];
+    }
+}
