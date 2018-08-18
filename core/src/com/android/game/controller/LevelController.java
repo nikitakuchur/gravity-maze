0d56a1793ca1702145b089ad97d0c042e4fa3063
Nikita Kuchur <nikitakuchur@gmail.com>
2018-08-18 14:17:06 +0300

    Added LevelController class

---
+++ core/src/com/android/game/controller/LevelController.java
@@ -0,0 +1,14 @@
+package com.android.game.controller;
+
+import com.android.game.model.Level;
+
+public class LevelController {
+    private Level level;
+
+    public LevelController(Level level) {
+        this.level = level;
+    }
+
+    public void update() {
+    }
+}
