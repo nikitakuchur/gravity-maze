5693debfdee2d169568f11f2e40f45dcd22be253
Nikita Kuchur <nikitakuchur@gmail.com>
2018-08-18 14:21:09 +0300

    Updated

--- core/src/com/android/game/GameScreen.java
+++ core/src/com/android/game/GameScreen.java
@@ -1,19 +1,32 @@
 package com.android.game;
 
+import com.android.game.controller.LevelController;
+import com.android.game.model.Level;
+import com.android.game.view.LevelRenderer;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 
-public class GameScreen extends ScreenAdapter{
+public class GameScreen extends ScreenAdapter {
+
+    private Level level;
+    private LevelRenderer levelRenderer;
+    private LevelController levelController;
 
     @Override
     public void show() {
+        level = new Level();
+        levelRenderer = new LevelRenderer(level);
+        levelController = new LevelController(level);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        levelController.update();
+        levelRenderer.draw();
     }
 
     @Override
