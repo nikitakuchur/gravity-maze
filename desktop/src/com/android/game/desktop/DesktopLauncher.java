943ba492263b8b2c138f467790169d46491b138c
Nikita Kuchur <nikitakuchur@gmail.com>
2018-08-21 14:04:17 +0300

    Upadted DesktopLauncher class

--- desktop/src/com/android/game/desktop/DesktopLauncher.java
+++ desktop/src/com/android/game/desktop/DesktopLauncher.java
@@ -9,6 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.height = 800;
 		config.width = 450;
+		config.resizable = false;
 		new LwjglApplication(new PuzzleGame(), config);
 	}
 }
