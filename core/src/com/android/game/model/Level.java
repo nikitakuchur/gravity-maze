f0c8c9c5f9d33509aa81f9e3863fd02ea24af895
Nikita Kuchur <nikitakuchur@gmail.com>
2019-01-27 23:42:25 +0300

    Updated Level class

--- core/src/com/android/game/model/Level.java
+++ core/src/com/android/game/model/Level.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector2;
 
 import com.badlogic.gdx.graphics.Color;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Level {
 
@@ -20,7 +21,7 @@ public class Level {
                               {1, 1, 0, 0, 0, 0, 1, 1},
                               {1, 1, 0, 0, 0, 0, 1, 1}};
 
-    private ArrayList<Ball> balls;
+    private List<Ball> balls;
 
     private float rotation;
     private float scale;
@@ -39,40 +40,79 @@ public class Level {
         scale = 1;
     }
 
+    /**
+     * @return the width of the map
+     */
     public int getWidth() {
         return cells.length;
     }
 
+    /**
+     * @return the height of the map
+     */
     public int getHeight() {
         return cells[0].length;
     }
 
+    /**
+     * Returns the id of the cell
+     *
+     * @param x the x-component
+     * @param y the y-component
+     * @return the id
+     */
     public int getCellId(int x, int y) {
         if ( x >= cells.length || x < 0 || y >= cells[0].length || y < 0)
             return 1;
         return cells[x][y];
     }
 
-    public ArrayList<Ball> getBalls() {
+    /**
+     * @return the list of the balls
+     */
+    public List<Ball> getBalls() {
         return balls;
     }
 
-    public void setRotation(float rotation) {
+    /**
+     * Sets the rotation of the map
+     * @param angle the angle
+     */
+    public void setRotation(float angle) {
         this.rotation = rotation;
     }
 
+    /**
+     * @return the rotation of the map
+     */
     public float getRotation() {
         return rotation;
     }
 
+    /**
+     * Sets the scale of the map
+     * @param scale the scale
+     */
     public void setScale(float scale) {
         this.scale = scale;
     }
 
+    /**
+     * @return the scale of the map
+     */
     public float getScale() {
         return scale;
     }
 
+    /**
+     * Returns the background colors
+     * 0 - bottom left
+     * 1 - bottom right
+     * 2 - top right
+     * 3 - top left
+     *
+     * @return the array of the colors
+     */
     public Color[] getBackgroundColor() {
         return new Color[]{backgroundColor[0].cpy(),
                            backgroundColor[1].cpy(),
@@ -80,6 +120,9 @@ public class Level {
                            backgroundColor[3].cpy()};
     }
 
+    /**
+     * @return the color of the map
+     */
     public Color getColor() {
         return color.cpy();
     }
