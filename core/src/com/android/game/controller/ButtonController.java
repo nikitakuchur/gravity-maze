52bb3752850c73034cbc1b094822aa52c569dd34
Nikita Kuchur <nikitakuchur@gmail.com>
2019-02-10 02:36:24 +0300

    Added ButtonController

---
+++ core/src/com/android/game/controller/ButtonController.java
@@ -0,0 +1,37 @@
+package com.android.game.controller;
+
+import com.android.game.model.Button;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+
+public class ButtonController implements Controller {
+
+    private Button button;
+
+    public ButtonController(Button button) {
+        this.button = button;
+    }
+
+    public void setButton(Button button) {
+        this.button = button;
+    }
+
+    public Button getButton() {
+        return button;
+    }
+
+    public void checkClick(Vector2 position) {
+        if (button.getPosition().x <= position.x &&
+            button.getPosition().x + button.getSize().x >= position.x &&
+            button.getPosition().y <= Gdx.graphics.getHeight() - position.y &&
+            button.getPosition().y + button.getSize().y >= Gdx.graphics.getHeight() - position.y)
+            button.setPressed(!button.isPressed());
+        else
+            button.setPressed(false);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+
+    }
+}
--- core/src/com/android/game/controller/GameController.java
+++ core/src/com/android/game/controller/GameController.java
@@ -1,19 +1,35 @@
 package com.android.game.controller;
 
+import com.android.game.model.Button;
 import com.android.game.model.Game;
 import com.badlogic.gdx.math.Vector2;
+
+import java.util.List;
 
 public class GameController implements Controller {
 
     private MapController mapController;
 
+    private ButtonController buttonController;
+    private List<Button> buttons;
+
     public GameController(Game game) {
         mapController = new MapController(game.getMap());
+
+        buttons = game.getButtons();
+
+        if (buttons.size() > 0)
+            buttonController = new ButtonController(buttons.get(0));
     }
 
     @Override
     public void update(float deltaTime) {
         mapController.update(deltaTime);
+
+        for (Button button : buttons) {
+            buttonController.setButton(button);
+            buttonController.update(deltaTime);
+        }
     }
 
     /**
@@ -22,6 +38,12 @@ public class GameController implements Controller {
      * @param position the position
      */
     public void touchDown(Vector2 position) {
+        for (Button button : buttons) {
+            buttonController.setButton(button);
+            if (!button.isPressed())
+                buttonController.checkClick(position);
+        }
+
         mapController.startMapRotation(position);
     }
 
@@ -31,6 +53,12 @@ public class GameController implements Controller {
      * @param position the position
      */
     public void touchUp(Vector2 position) {
+        for (Button button : buttons) {
+            buttonController.setButton(button);
+            if (button.isPressed())
+                buttonController.checkClick(position);
+        }
+
         mapController.stopMapRotation();
     }
 
