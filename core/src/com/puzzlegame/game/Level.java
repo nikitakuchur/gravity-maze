d44a89c836bdc044dd168099be970a1100a0f28a
Nikita Kuchur <nikitakuchur@gmail.com>
2019-07-19 23:26:14 +0300

    Removed import

--- core/src/com/puzzlegame/game/Level.java
+++ core/src/com/puzzlegame/game/Level.java
@@ -1,7 +1,6 @@
 package com.puzzlegame.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.utils.Disposable;
