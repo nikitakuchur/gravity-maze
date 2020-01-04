7e18a365696c496554ecc5c70d237385790238f8
Nikita Kuchur <nikitakuchur@gmail.com>
2020-01-04 22:37:46 +0300

    Small fix

--- editor/src/com/github/nikitakuchur/puzzlegame/editor/LevelEditor.java
+++ editor/src/com/github/nikitakuchur/puzzlegame/editor/LevelEditor.java
@@ -72,7 +72,7 @@ public class LevelEditor extends Group implements Disposable {
         level.act(0);
         level.setPause(true);
         levelChangeListeners.forEach(Runnable::run);
-        selectedGameObject = null;
+        setSelectedGameObject(null);
     }
 
     public GameObject getSelectedGameObject() {
