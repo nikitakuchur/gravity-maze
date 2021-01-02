850cae0735445a410e3164d231044709d1f95c9b
Nikita Kuchur <nikitakuchur@gmail.com>
2021-01-02 05:36:10 +0300

    Updated level serialization

--- core/src/com/github/nikitakuchur/puzzlegame/game/entities/Parameterizable.java
+++ core/src/com/github/nikitakuchur/puzzlegame/game/entities/Parameterizable.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.github.nikitakuchur.puzzlegame.utils.Parameters;
 import com.google.gson.*;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Type;
 
 public interface Parameterizable {
@@ -12,47 +13,49 @@ public interface Parameterizable {
 
     void setParameters(Parameters parameters);
 
-    // TODO: Refactor this class, please
     class Serializer implements JsonSerializer<Parameterizable>, JsonDeserializer<Parameterizable> {
+
+        private static final String CLASS_FIELD = "class";
+        private static final String VALUE_FIELD = "value";
 
         @Override
         public JsonElement serialize(Parameterizable parameterizable, Type typeOfSrc, JsonSerializationContext context) {
             Parameters parameters = parameterizable.getParameters();
             JsonObject root = new JsonObject();
-            root.addProperty("class", parameterizable.getClass().getName());
-            parameters.nameSet().forEach(name -> {
-                JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("type", parameters.getType(name).getName());
-                jsonObject.add("value", context.serialize(parameters.getValue(name)));
-                root.add(name, jsonObject);
-            });
+            root.addProperty(CLASS_FIELD, parameterizable.getClass().getName());
+            JsonObject valueObject = new JsonObject();
+            parameters.nameSet().forEach(name -> valueObject.add(name, context.serialize(parameters.getValue(name))));
+            root.add(VALUE_FIELD, valueObject);
             return root;
         }
 
         @Override
         public Parameterizable deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
-            Parameters parameters = new Parameters();
             JsonObject jsonObject = json.getAsJsonObject();
             try {
-                Class<?> clazz = Class.forName(jsonObject.get("class").getAsString());
-                jsonObject.remove("class");
-                jsonObject.keySet().forEach(key -> {
-                    JsonObject property = jsonObject.get(key).getAsJsonObject();
-                    try {
-                        Class<?> type = Class.forName(property.get("type").getAsString());
-                        Object value = context.deserialize(property.get("value"), type);
-                        parameters.put(key, type, value);
-                    } catch (ClassNotFoundException e) {
-                        Gdx.app.error("Parameterizable.Serializer", e.toString());
-                    }
-                });
-                Parameterizable parameterizable = (Parameterizable) clazz.getConstructor().newInstance();
-                parameterizable.setParameters(parameters);
-                return parameterizable;
+                Class<?> clazz = Class.forName(jsonObject.get(CLASS_FIELD).getAsString());
+                JsonElement valueElement = jsonObject.get(VALUE_FIELD);
+                if (valueElement.isJsonObject()) {
+                    JsonObject valueObject = valueElement.getAsJsonObject();
+                    return deserializeValue(valueObject, clazz, context);
+                }
             } catch (Exception e) {
                 Gdx.app.error("Parameterizable.Serializer", e.toString());
             }
             return null;
+        }
+
+        private Parameterizable deserializeValue(JsonObject valueObject, Class<?> clazz, JsonDeserializationContext context)
+                throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+            Parameterizable parameterizable = (Parameterizable) clazz.getConstructor().newInstance();
+            Parameters parameters = parameterizable.getParameters();
+            for (String name : parameters.nameSet()) {
+                Class<?> type = parameters.getType(name);
+                Object obj = context.deserialize(valueObject.get(name), type);
+                parameters.put(name, type, obj);
+            }
+            parameterizable.setParameters(parameters);
+            return parameterizable;
         }
     }
 }
