af14fa32b2165a4f2a88536ed5b24f8cf4258d0d
Nikita Kuchur <nikitakuchur@gmail.com>
2021-01-29 01:23:52 +0300

    Updated menu background

--- core/src/com/github/nikitakuchur/puzzlegame/ui/menu/MenuBackground.java
+++ core/src/com/github/nikitakuchur/puzzlegame/ui/menu/MenuBackground.java
@@ -15,29 +15,27 @@ public class MenuBackground extends Background {
 
     private final ShapeRenderer shapeRenderer = new ShapeRenderer();
 
-    private final List<Pattern> patterns = new ArrayList<>();
+    private final List<Figure> figures = new ArrayList<>();
 
-    private static final int PATTERN_SIZE = 6;
     private float timer = 0.f;
 
     public MenuBackground(Color firstColor, Color secondColor) {
         super(firstColor, secondColor);
-        patterns.add(new Pattern(getColor()));
+        figures.add(Figure.randomFigure(getColor()));
     }
 
     @Override
     public void act(float delta) {
         super.act(delta);
-        patterns.forEach(pattern -> pattern.update(delta));
+        figures.forEach(figure -> figure.update(delta));
 
-        if (timer > 3.f) {
-            patterns.add(new Pattern(getColor()));
+        if (timer > 0.1f) {
+            figures.add(Figure.randomFigure(getColor()));
             timer = 0.f;
-            if (patterns.size() > PATTERN_SIZE) {
-                patterns.remove(0);
-            }
         }
         timer += delta;
+
+        figures.removeIf(Figure::isDead);
     }
 
     @Override
@@ -49,73 +47,27 @@ public class MenuBackground extends Background {
         shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         Gdx.gl20.glLineWidth(Gdx.graphics.getWidth() / 300f);
-        patterns.forEach(pattern -> pattern.draw(shapeRenderer));
+        figures.forEach(figure -> figure.draw(shapeRenderer));
         shapeRenderer.end();
         Gdx.gl.glDisable(GL20.GL_BLEND);
         batch.begin();
     }
 
-    private static class Pattern extends Figure {
+    private static class Figure {
 
         private static final Random random = new Random();
-        private final List<Figure> figures = new ArrayList<>();
 
-        private final float sizeFactor;
-        private final float rotationFactor;
-
-        private final float time;
-
-        public Pattern(Color color) {
-            super(
-                    random.nextFloat() * Gdx.graphics.getWidth() - (float) Gdx.graphics.getWidth() / 2,
-                    random.nextFloat() * Gdx.graphics.getHeight() - (float) Gdx.graphics.getHeight() / 2,
-                    (float) Gdx.graphics.getWidth() / 16 + random.nextFloat() * Gdx.graphics.getWidth() / 16,
-                    15 + random.nextFloat() * 30.f,
-                    3 + Math.abs(random.nextInt() % 4), color.cpy());
-            sizeFactor = 1.5f + random.nextFloat() / 2;
-            rotationFactor = 15 + random.nextFloat() * 15.f;
-            time = 0.5f + random.nextFloat();
-        }
-
-        @Override
-        public void update(float delta) {
-            figures.forEach(figure -> figure.update(delta));
-            if (timer > time) {
-                expand();
-                timer = 0;
-            }
-            timer += delta;
-        }
-
-        public void expand() {
-            float size = this.size;
-            float rotation = this.rotation;
-            if (!figures.isEmpty()) {
-                Figure lastFigure = figures.get(figures.size() - 1);
-                size = lastFigure.size * sizeFactor;
-                rotation = lastFigure.rotation + rotationFactor;
-            }
-            figures.add(new Figure(x, y, size, rotation, segments, color.cpy()));
-        }
-
-        @Override
-        public void draw(ShapeRenderer shapeRenderer) {
-            for (Figure figure : figures) {
-                figure.draw(shapeRenderer);
-            }
-        }
-    }
-
-    private static class Figure {
-        protected final float x;
-        protected final float y;
+        protected float x;
+        protected float y;
 
         protected float size;
-        protected final float rotation;
+        protected float rotation;
         protected final int segments;
         protected final Color color;
 
         protected float timer = 0;
+
+        private final float rotationFactor;
 
         public Figure(float x, float y, float size, float rotation, int segments, Color color) {
             this.x = x;
@@ -123,7 +75,9 @@ public class MenuBackground extends Background {
             this.size = size;
             this.rotation = rotation;
             this.segments = segments;
-            this.color = color;
+            this.color = color.cpy();
+
+            rotationFactor = 1 - (random.nextFloat() * 2);
         }
 
         public void update(float delta) {
@@ -132,13 +86,30 @@ public class MenuBackground extends Background {
             color.a -= timer / 300;
             if (color.a < 0) color.a = 0;
 
-            size -= size / 40 * timer / 6;
+            size -= (float) Gdx.graphics.getWidth() / 64 * delta;
             if (size < 0) size = 0;
+
+            y -= (float) Gdx.graphics.getWidth() / 32 * delta;
+            rotation += 15 * rotationFactor * delta;
         }
 
         public void draw(ShapeRenderer shapeRenderer) {
             shapeRenderer.setColor(color);
             shapeRenderer.ellipse(x - size / 2, y - size / 2, size, size, rotation, segments);
+        }
+
+        public boolean isDead() {
+            return color.a == 0;
+        }
+
+        public static Figure randomFigure(Color color) {
+            float c = (0.5f - random.nextFloat()) / 16;
+            return new Figure(
+                    random.nextFloat() * Gdx.graphics.getWidth() - (float) Gdx.graphics.getWidth() / 2,
+                    random.nextFloat() * Gdx.graphics.getHeight() - (float) Gdx.graphics.getHeight() / 2,
+                    (float) Gdx.graphics.getWidth() / 24 + random.nextFloat() * Gdx.graphics.getWidth() / 8,
+                    random.nextFloat() * 360.f,
+                    3 + Math.abs(random.nextInt() % 4), color.cpy().add(c, c, c, 0.8f));
         }
     }
 }
