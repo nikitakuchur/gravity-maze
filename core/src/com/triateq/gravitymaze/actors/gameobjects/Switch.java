2101c3f6ba911259f53cb09a6dc8f96f1205d064
Nikita Kuchur <nikitakuchur@gmail.com>
2021-03-21 20:00:05 +0300

    Added switch texture

--- core/src/com/triateq/gravitymaze/actors/gameobjects/Switch.java
+++ core/src/com/triateq/gravitymaze/actors/gameobjects/Switch.java
@@ -1,15 +1,27 @@
 package com.triateq.gravitymaze.actors.gameobjects;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.triateq.gravitymaze.utils.Context;
 
 public class Switch extends GameObject {
 
-    private final ShapeRenderer shapeRenderer = new ShapeRenderer();
+    private final TextureRegion switchTextureRegion;
+    private final TextureRegion glowTextureRegion;
 
     private Ball lastBall;
     private boolean activated;
+
+    public Switch(Context context) {
+        AssetManager assetManager = context.getAssetManager();
+        switchTextureRegion = new TextureRegion(assetManager.get("textures/switch/switch.png", Texture.class));
+        glowTextureRegion = new TextureRegion(assetManager.get("textures/switch/glow.png", Texture.class));
+        setColor(Color.WHITE.cpy());
+    }
 
     @Override
     public void act(float delta) {
@@ -36,17 +48,20 @@ public class Switch extends GameObject {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-        batch.end();
-        shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
-        shapeRenderer.setTransformMatrix(batch.getTransformMatrix());
 
+        if (!activated) {
+            switchTextureRegion.setRegion(0, 0, 512, 512);
+            glowTextureRegion.setRegion(0, 0, 512, 512);
+        } else {
+            switchTextureRegion.setRegion(512, 0, 512, 512);
+            glowTextureRegion.setRegion(512, 0, 512, 512);
+        }
         Vector2 position = getActualPosition();
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        shapeRenderer.setColor(getColor());
-        shapeRenderer.ellipse(position.x, position.y, getWidth(), getHeight(), 3);
-
-        shapeRenderer.identity();
-        shapeRenderer.end();
-        batch.begin();
+        batch.setColor(getColor());
+        batch.draw(switchTextureRegion, position.x, position.y, getOriginX(), getOriginY(),
+                getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+        batch.setColor(Color.WHITE);
+        batch.draw(glowTextureRegion, position.x, position.y, getOriginX(), getOriginY(),
+                getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
     }
 }
